d5bc42db695625b23add93e155bdde0d22fc7443
Marin-Andrei <andreimarinjunior@gmail.com>
2024-08-21 14:52:50 +0300

    replaced code.sandbox w/ travel list

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import Logo from "./components/Logo";
+import Form from "./components/Form";
+import PackingList from "./components/PackingList";
+import Stats from "./components/Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    if (items.length) {
+      const confirmed = window.confirm(
+        "Are you sure you want to delete all items?"
+      );
+
+      if (confirmed) setItems([]);
+    }
+  }
+  // daca vreau sa dau un state de la parinte la copil, il impachetez in props => handleAppItems devin onAddItems={handleAddItems}
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,106 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./styles.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const skills = [
-  {
-    skill: "HTML+CSS",
-    level: "advanced",
-    color: "#2662EA",
-  },
-  {
-    skill: "JavaScript",
-    level: "advanced",
-    color: "#EFD81D",
-  },
-  {
-    skill: "Web Design",
-    level: "advanced",
-    color: "#C3DCAF",
-  },
-  {
-    skill: "Git and GitHub",
-    level: "intermediate",
-    color: "#E84F33",
-  },
-  {
-    skill: "React",
-    level: "advanced",
-    color: "#60DAFB",
-  },
-  {
-    skill: "Svelte",
-    level: "beginner",
-    color: "#FF3B00",
-  },
-];
-
-function App() {
-  return (
-    <div className="card">
-      <Avatar />
-      <div className="data">
-        <Intro />
-
-        <SkillList />
-      </div>
-    </div>
-  );
-}
-
-function Avatar() {
-  return (
-    <img className="avatar" src="image_react.jpg" alt="Reaching for the sun" />
-  );
-}
-
-function Intro() {
-  return (
-    <div>
-      <h1>Mario Crezzulinni</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
-        quibusdam sunt consectetur soluta magni aspernatur recusandae distinctio
-        error.
-      </p>
-    </div>
-  );
-}
-
-function SkillList() {
-  return (
-    <ul className="skill-list">
-      {skills.map((respectiveSkill) => (
-        <Skill
-          skillName={respectiveSkill.skill}
-          skillLevel={respectiveSkill.level}
-          skillColor={respectiveSkill.color}
-        />
-      ))}
-    </ul>
-  );
-}
-
-function Skill(props) {
-  return (
-    <li className="skill" style={{ backgroundColor: props.skillColor }}>
-      <span>{props.skillName}</span>
-      <span>
-        {props.skillLevel === "advanced"
-          ? "💪"
-          : props.skillLevel === "intermediate"
-          ? "👍"
-          : "👶"}
-      </span>
-    </li>
-  );
-}
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
